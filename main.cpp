b1374e4656a1c93dbb2539337178fc78265993cd
= <=>
2025-12-04 11:28:05 +0500

    branch changes

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+
+using namespace std;
+
+int main() {
+    cout << "Hello, World!" << endl;
+    cout<< "This project uses the C++ language standard version." << endl;
+    return 0;
+}

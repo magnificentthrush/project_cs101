a6cc50ff698e8c69595d7e99a6334b1d01f54569
= <=>
2025-12-04 11:38:32 +0500

    new_code change 2

--- main.cpp
+++ main.cpp
@@ -5,6 +5,6 @@ using namespace std;
 int main() {
     cout << "Hello, World!" << endl;
     cout<< "This project uses the C++ language standard version." << endl;
-    cout<<"new_code branch changes"<<endl;
+    cout<<"new_code branch change 2"<<endl;
     return 0;
 }

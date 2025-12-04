b944b21d0344f7cea8a28770ff3a6c0f854f05a4
= <=>
2025-12-04 11:31:04 +0500

    main changed to show conflict

--- main.cpp
+++ main.cpp
@@ -5,5 +5,6 @@ using namespace std;
 int main() {
     cout << "Hello, World!" << endl;
     cout<< "This project uses the C++ language standard version." << endl;
+    cout<<"changed main.cpp file"<<endl;
     return 0;
 }

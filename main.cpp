24d714da29f800662c75fcf87b5aecee511d710c
yawarr <yawarkmaal111@gmail.com>
2025-12-04 12:34:02 +0500

    changed email

--- main.cpp
+++ main.cpp
@@ -8,6 +8,6 @@ int main() {
     cout<<"changed main.cpp file"<<endl;
     cout<<"new_code branch change 2"<<endl;
     cout<<"user name changed"
-    cout<<"email test"
+    cout<<"email changed"
     return 0;
 }

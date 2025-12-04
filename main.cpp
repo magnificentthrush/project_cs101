eb6cf66e2372bc05781aa4e90d3e47227cd3fb5a
= <=>
2025-12-04 11:31:47 +0500

    new_code changed to show conflict

--- main.cpp
+++ main.cpp
@@ -5,5 +5,6 @@ using namespace std;
 int main() {
     cout << "Hello, World!" << endl;
     cout<< "This project uses the C++ language standard version." << endl;
+    cout<<"new_code branch changes"<<endl;
     return 0;
 }
